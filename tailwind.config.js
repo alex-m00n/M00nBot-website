460a1ff94b244d5c974f03ccfa9cfcbaa2de53a7
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2025-05-31 11:38:00 +0200

    Little Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,15 @@ module.exports = {
           '100%': { transform: 'translate(-50%, -50%) translate(0, 0)' },
         },
       },
+      colors: {
+        primary: '#0062ffff',
+        primary2: '#0062ffc0',
+        secondary: '#5d00ffff',
+        secondary2: '#5d00ffc0',
+
+      },
     },
   },
+  variants: {},
   plugins: [],
 } 
